5724e9a2add05a1c0f08705512cb4d5dc9040ed9
Chris Stiles <chrisstiles92@gmail.com>
2020-02-11 17:44:00 -0800

    Continue UI

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,18 @@ import styles from './Header.module.scss';
 export default function Header() {
   return (
     <div className={styles.wrapper}>
-      Header here
+      <div className={styles.content}>
+        <Top />
+        {(new Date().toISOString())}
+      </div>
     </div>
   )
+}
+
+function Top() {
+  return (
+    <div className={styles.top}>
+      <h1>Today's Mortgage Rates</h1>
+    </div>
+  );
 }
