0ca63b96d3e58d6923193ffaa86473f28446e62a
chrisstiles <chrisstiles92@gmail.com>
2020-03-02 09:53:58 -0800

    Add configuration for default tab order

--- src/components/Header/LoanTypeTabs/LoanTypeTabs.js
+++ src/components/Header/LoanTypeTabs/LoanTypeTabs.js
@@ -1,35 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './LoanTypeTabs.module.scss';
 import { Purchase, Refinance } from '../icons';
 import Label from '@components/Label';
 import classNames from 'classnames';
+import config from '@config';
 
 export default function LoanTypeTabs({
   loanType,
   setLoanType
 }) {
   const purchaseIsActive = loanType === 'purchase';
+  const [firstTab] = useState(() => {
+    const { defaultLoanType = 'refinance', displayDefaultTabFirst = true } = config;
+
+    if (displayDefaultTabFirst) {
+      return defaultLoanType;
+    }
+
+    return 'purchase';
+  });
+
+  const purchase = (
+    <Tab
+      loanType="purchase"
+      isActive={purchaseIsActive}
+      setLoanType={setLoanType}
+      key="purchase"
+    >
+      <Purchase className={styles.purchaseIcon} /> Purchase
+    </Tab>
+  );
+
+  const refinance = (
+    <Tab
+      loanType="refinance"
+      isActive={!purchaseIsActive}
+      setLoanType={setLoanType}
+      key="refinance"
+    >
+      <Refinance /> Refinance
+    </Tab>
+  );
+
+  const tabs = [purchase];
+
+  if (firstTab === 'purchase') {
+    tabs.push(refinance);
+  } else {
+    tabs.unshift(refinance);
+  }
 
   return (
     <div className={styles.wrapper}>
       <Label>What type of loan are you looking for?</Label>
       <div className={styles.tabWrapper}>
-        <Tab
-          loanType="purchase"
-          isActive={purchaseIsActive}
-          setLoanType={setLoanType}
-        >
-          <Purchase className={styles.purchaseIcon} />
-          Purchase
-        </Tab>
-        <Tab
-          loanType="refinance"
-          isActive={!purchaseIsActive}
-          setLoanType={setLoanType}
-        >
-          <Refinance />
-          Refinance
-        </Tab>
+        {tabs}
       </div>
     </div>
   );
