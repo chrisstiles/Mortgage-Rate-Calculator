c04aaf1948e85969403e0c9dbd043e8f5809702a
Chris Stiles <chrisstiles92@gmail.com>
2020-03-09 11:56:19 -0700

    Add react-text-mask

--- src/components/input/Text/Text.js
+++ src/components/input/Text/Text.js
@@ -2,22 +2,25 @@ import React, { useState } from 'react';
 import { Field } from '@input';
 import classNames from 'classnames';
 import styles from './Text.module.scss';
+import MaskedInput from 'react-text-mask';
 
 export default function Text({
   className,
   fieldClassName,
   label,
+  type = 'text',
   icon,
   iconStyle = {},
   iconLeft,
   insetLeft,
   iconClassName,
+  mask = false,
   style = {},
   fieldStyle,
   ...restProps
 }) {
   const [isFocused, setIsFocused] = useState(false);
-  
+ 
   return (
     <Field
       label={label}
@@ -38,8 +41,10 @@ export default function Text({
             {icon}
           </div>
         }
-        <input
+        <MaskedInput
           type="text"
+          mask={mask}
+          guide={false}
           className={classNames(styles.input, className)}
           style={{ ...style, paddingLeft: insetLeft }}
           onFocus={() => setIsFocused(true)}
