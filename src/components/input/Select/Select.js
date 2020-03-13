24558068320ef7ec43285d1f6d33d01750c4fe11
chrisstiles <chrisstiles92@gmail.com>
2020-03-13 10:45:12 -0700

    Continue working on select box

--- src/components/input/Select/Select.js
+++ src/components/input/Select/Select.js
@@ -1,9 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Field } from '@input';
+import styles from './Select.module.scss';
+import classNames from 'classnames';
 
-export default function Select({ options, value }) {
+export default function Select({
+  options,
+  value,
+  label,
+  maxWidth,
+  fieldClassName,
+  fieldStyle,
+  tabIndex = 0,
+}) {
+  const [isFocused, setIsFocused] = useState(false);
+
   return (
-    <select>
-      <option>Testing</option>
-    </select>
+    <Field
+      label={label}
+      className={fieldClassName}
+      style={fieldStyle}
+      maxWidth={maxWidth}
+    >
+      <div
+        className={classNames(styles.wrapper, {
+          [styles.focus]: isFocused
+        })}
+        tabIndex={tabIndex}
+        onClick={() => setIsFocused(true)}
+      >
+        Excellent Credit
+        <Options
+
+        />
+      </div>
+      {/* <select>
+        <option>Testing</option>
+      </select> */}
+    </Field>
+  );
+}
+
+function Options() {
+  return (
+    <div>Test</div>
   );
 }
