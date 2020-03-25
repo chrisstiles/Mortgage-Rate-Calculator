8c75afe63f3367ae30bedf64a1dec5b0ccc2996c
Chris Stiles <chrisstiles92@gmail.com>
2020-03-25 09:38:44 -0700

    Transpile react-spring

--- webpack.config.js
+++ webpack.config.js
@@ -75,7 +75,7 @@ module.exports = (env, argv) => {
       rules: [
         {
           test: /\.m?js$/,
-          exclude: /node_modules\/(?!unfetch\/)/,
+          exclude: /node_modules\/(?!unfetch\/)\/(?!react-spring\/)/,
           use: {
             loader: 'babel-loader',
             options: {
