24efc3ce991a535308f122684c3bc5435074ac89
Chris Stiles <chrisstiles92@gmail.com>
2020-08-07 15:47:53 -0700

    Change loading bar animation

--- src/config.js
+++ src/config.js
@@ -18,7 +18,7 @@ module.exports = {
   // Whether or not to use sample data to prevent
   // continuously loading data from MORRIS API in
   useSampleData: false,
-  sampleLoadingTime: 2000,
+  sampleLoadingTime: 20000,
 
   /*-- API Endpoint URI --*/
 
